30ec35b95b69654c67b4331d2d6f30c59ba11cb6
joporyk <jo.dio@bk.ru>
2022-09-27 13:31:02 +0300

    test
    test

---
+++ №1/task_2.R
@@ -0,0 +1,5 @@
+#объединение векторов
+vector_1 <- c(1 : 5000)
+vector_2 <- c(7000 : 10000)
+the_best_vector = c(vector_1, vector_2)
+
---
+++ №1/task_3.R
@@ -0,0 +1,4 @@
+#сумма векторов
+my_sum = my_vector[my_vector > 10]
+sum(my_sum)
+
